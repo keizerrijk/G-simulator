2b83d20865daf40d680168b5654392fa09a68f70
zoomzer <66572381+zoomzer@users.noreply.github.com>
2020-11-29 02:40:35 +0100

    Create images.h

---
+++ libs/images.h
@@ -0,0 +1,197 @@
+const char* cheems = R"(                                           
+                           .----.                                                                   
+                          /syyyyys//////+///::---.....`                                             
+                         `syyssssyyyyysssssyyyyyyyyyyyyysooooss+:.                                  
+                         :sssyyyyyyyyyyyyyyyyyyhhhdddhhhhyyyyyyhhhy:                                
+                      `:osssyyyyyyyyyyyyyyyyyyhhhhdmmddhyyyhhhyyyhdds:`                             
+                     -oooossyhhhhyyyyyyyyyyyyyyhhhhhddhhyyhhdddhyhdmmdyo:                           
+                   `+ooooosyhhhhyyyyyyyyyyyyyyyhhhyyyhhyyyhdddddhyhdmmddhs.                         
+                  `o+/+oosyhhhhyyyyyyyyyyyyyyyyyhhhyyyyhhyhhyhdhyyyhmmmddhh/`                       
+                  +yo+ooosyyhyyyssssssyyyyyyyyyyhhhhhyyhhhhyyyhysyydmmmdddhhs-                      
+                 -hhyo+oosyysssssssssooosssyyyyhhhhhhhyyhhdhhhhhysymmmmmdddhhs-                     
+                 oNNmsoossssssosyhhdhhhhhyyyhhhhhhhhhhhyyyhdddddddmmNmmdddhhhhy:                    
+                `yddhssyyyyssyhdmNNNNNNNNmhysssyyyyyyyyyyyyhddddddmmmmdddhhhhhhy:                   
+                -syhhhhhhddddddhysyyhhyyoo+++///+ossyyyyyyyyhhhhhhddddddhhhhhhhhs.                  
+                /hddddddddddddyso++ooooo+++//::::/+osssyyyyhhhhhhhhhhhhhhhhhhhhhhy-                 
+              -sdddmmmddddhhhyso++////////::::::://++osssyyyyhhhhhhhyhhhhyyhhhhhhhho-               
+            :yddmmmmdhhhyyssssoo+//////::::--::::///++oossyyyyhhyyyyyhhhyyyyyhhhhhddho.             
+          -hdmmmmmmmdhhyssosssso+++++////:::::::::://+++oossyyyyyyyyhhhhyyyhhhhhhhhdhhho-`          
+        `+mNNNNNNNNmdyssssssyyyoosysso++///:::::://///+++oossyyyyhhhhhhyyyyhhhhhhhhhhhhhhs/.        
+        omNNNNNNNNNNdysssyssssyyyddhyys+++/++////////++oooossyyyhhhhhhhhhhhhhhhhhhhhhhhyhhys-       
+        :hNNNMMNNNmdhyyssossyyyhmNNmdyso+//::///////+++oosssyyyyhhhhhhhhhhhhhhhhhhhhhhhhhhyy+       
+         .odmNNNmmmdhyyyyyhhhdmNNmdhysso+///:::///++++ooosssyyyhhhhhhhhhddddhhhhhhhhhhhhhhyyo.      
+           .:+ydmmmmmdddhdddmmdhyyysso++//////////+++oosssyyyyyyhhhhhhdddddhhhhhhhhhhhhhhhhhy+.     
+                   `./yyhhhhyyyssso++////////////++oosssssyyyhhhhhhhhdddddhhhhhhyyhhhhhyyyyhhys/.   
+                      :ssssssssoo+++////////////+++oossyyyyhhhhhdhhhddddhhhhhhhhhhyyyyyhhhhhhhhhhs- 
+                      .+oooooo++++////////////+++oosssyyyhhhhhhhhhhddddhhhhhhhyyyhyyyhhhyyyyhhddddhs
+                      ./+++++++///////////+++++oossyyyyhhhhhhhhhhhddhhhhhhhyyyyyyhhhhhhyyyyhhddddddd
+                      -/++++++++++++++++++++ooossssyyhhhhhhhhhhhhhhhhyyyyyyyyyyyyyyyyyyyyhhhdddddddd
+                      :++++++++++++////++++oooosssyyyyyyyhhhhhhhhhhyyyyyyyyyyyyyyyyyyyyyyhdddddddddd
+                      :+++//+++++++//////+++ooosssssyyyyyyyyyyyyyyyssssssssssssyyyyyyhhhdddddddddddd
+                      :+++//////+//////////+++oooosssssyyyssssssssssossoooossssyyyyyhhdddddddddhdddd
+)";
+const char * walter = R"(
+                .:/::::-----:--:--.               
+             ./ydmmdhhddddmmmmmNNNNms.            
+           `/s+syhhysyhdddmmmNNNNNNNNd/`          
+          -/:::+sysosyhhddmmmmNNNNmdd++s+         
+         .:` .syys+oyhdddddmddmmNNNmm/ `o/        
+       ./sssosyyoooyhddddddhhdddmmNNNmo+sh+`      
+      ./oyyyyys//+syhhhdddhhhhdddmmNNNNmmmmh`     
+     .:/++ssso++syyhhyhhhhyyyyyhddmmNNNNNNNms     
+    -:://+so+++oo+::/+ooooso:..-/shmmNNNNNNNmo    
+   .:////o+///:-.`   ````./-`````./sdmmNNNNNNm+   
+   :::::////::-`        ```````````-yddmNNNNNNm:  
+  .:---:::/:::.             ```````-shhmmmNmmmmd- 
+  ------:///:-`                   `.syhddmmmmmmm+ 
+  .-----:://:--`                  ./oyhddmmmmmmmo 
+  .-----:::/::-.               ```-osyhhdmmmmmdd+ 
+  .---:---:/:::.`              ```-osyyhhdmdmddd: 
+  `--------:::::.`              `./osyyhhdddmmdh` 
+   --------::::::-``           `./+ooosyhhddddds  
+   `--------:::---..````     ``-:/+++osyyhdddhh-  
+    .--........```````````` ``..-::/+oossyhhdh+   
+     -...`````       ````.`````````.-:/+syhhd+    
+     --....`..`````  ```.:-````````.//+oyhddo     
+     --...............-::/::--..`.-+osyhhddy      
+     .--............--:--::///////+oshhdddd+      
+     .---......--.-..-----::://+ososyhddddm/      
+     .---......--------::--:/++osyyyhdddmmmy      
+    `.---......-...-:::::///+osssyhddmmmmmmh`     
+    `.---..........--:://+osyyysyhdmmmmmmNdh.     
+    `.--------..--:/+osssyyhhddddmmmmmmmmmdh.     
+     .---::://:--:/+osyyhhhhddddmmmmmmmmmmdd.     
+     .::///+++/:::/+syyyhhhhdddmmmmmmmNNmmdm/     
+   ` -//++++ooo+//+osyyyyhhdddmmmmmmmNNNNmmm+     
+   )";
+const char * doge = R"(
+               `:/`                               
+               /hso`                    `:+/.     
+               +hsoo`                 `/yssoo.    
+               /hsoos-              `/yssso+++    
+               /yssssso:-.`````   `/ssoso+++o+`   
+              `+yyhhyyyyyyyyyyyyysss+oo+///+oo`   
+          `-/shhhhhyysssyyyyyyyyyso++oo+:-/ooo`   
+       `+hhddhhdddhysssyyyyyyyyyyyys+o+--:+so/    
+      .hddddhhhhdhhyssyyhhhyyysyyyyyyo/:/oo+o:    
+      yddddy+/+shhysssyhhhhhyyssyyyyyyyysoooo+    
+     +dmNmds:--shhhyhyyhys+/osysyyyyyyhhyyo++o    
+    -dmNmdhs+oyhhhhhhyo/-o+-..+syyyyyyyyyyys+s`   
+    ymmmmdhddhhdddhhhys+:+:-:oyhhhhhhhhhhyyyss.   
+   :mmmmmhysssyhhddhhhyyyyyyyyhhddddhhdhhhhyyy.   
+   smdmm/.....-/hdddhhhhhhhhhhhhddddddddhhhhyy.   
+  `dmmmh/-..```:yhdhhhhhdddddddddddddhhhhhhyy+    
+  .dmdhy+-...-/oyyhhhhhhhhhhhdddhhhhhhhhyyyys-    
+  `dmmhs+:--::/osyyyyyyyhhhhhhhhhhhhhhhhhyys+     
+   ymdddo:-....:+ssso++oyyhyyyhhhhhhhhhhhyy/      
+   .dmddhy+/////::///+oyhyyyhhhhhhhhhhhhys-       
+    -dddhhhysssyssyyyyyyyyyyyyhhhhhhhyyy+`        
+     :dddhhhhhhhyyyyyyyyyyyyhhhhhhhhyy+`          
+      .ohhhhhhhhhyyyyyyyyyhhhhhhhhyo:             
+        .+yhhhhhyyyyyyyyyyyyhhyyo:`               
+           -+yhyyyyyyyyyyyys+/-`                  
+              .-::/::::---. 
+	      )";
+const char * coomer = R"(
+                      /: --:`..-.                 
+                  -:-:osssyysyshs/``              
+       :- /:.::::+:///yhyyhsohho/.++///-`         
+       :soshs..      -+oysyyoyo. .` .:+/++        
+      /-`:-`           `:/+o/:       `://:o`      
+    ./.                    `           `   ::.    
+   ::                 --                     o    
+   o       `.-:-::-::-``:-:----:--:::        +`   
+  ::       --`-::---::-:----.----.`           /.  
+ ./       `-.-         ` `  `   `..---.        o  
+ o              -::::::.----:-::-::-----       /. 
+`+        -:::::`                      `        +.
+o`          .-----.             :::--::-:`      +.
+o     `:::--.                 .`         ::.    +.
+o     `            :-        /-         ```     .+
+o      ::-/:.`      -           .--/ohmo+s-      +
+o      .+/sMMMMh/:.            o::+MMMMm-:-     `o
+o      ``/odhhh:++:            +o+/ydmmo+o:     //
+/-     -:o+o/+/+/`              .+:::--/++`    `+ 
+ /.--     . .`.`.     :      /   `:--` ``    `-/` 
+ :+ +:               /-      +`             .so+/ 
+`h--`/           -:oo/        +-            /::ys 
+`ho/.+:+.     ./:`o            `:: ::.      /s/hs 
+ +oo//:+    ./.   o             -/   -/    `y/yhy 
+ .yhoy-o`  -:  -```/           .-`... `: .`:hyhh+ 
+  +hhyshoo+-::o/+/::-.-::-.-.---:++//-/o/hyyyyyh+`
+  :hhhyhhssso+s-++ ` `     ` `..-oh- `-/oshyshos/ 
+  `hhhhhyhhhooyo/s/:`           --`-s//yoohyhhy/  
+   s:oyhyhhyyhsyh:+so/---:----:..` +hoyoyyshyyy   
+   ````yhhhyhhhyhysy+/+``` `-:`:--yyhosohhhhys/   
+       .yyhhhhhy+yyhysy.s++y+/shyyohhyyhhhs:+/    
+         -//yhhhhyhhhyyhyyyshhyyyyyhyyhyh+`       
+            /yyhhhhhhsyhhyhyhyhyshyyhsy/-.        
+             ` `yhhhhyhhyhhhhshhshhho/`           
+                `:+o/+syhhhhyhhs+o-.              
+                        `.-.`..        
+			)";
+
+const char * peter = R"(
+                     `-------.                    
+                  `-:-..--...:o+                  
+                 .o/-:.-:-:.:.:.+..               
+                .-` ``-.` -....:`  --             
+                :     `:-.  ...:    `:            
+               ..       `.......:`   :            
+               :               -.    :            
+               :              `-.    ..           
+               :        .       `-   `-           
+              :-         -..-....    `/           
+          ....- ..                   ..:.         
+       ...     .. ..                -`.` ...      
+    `..         `.. `..`          .. -`     .-.`  
+  `:-..`           ... `.....  .-. ..  -      --- 
+ :`     `...          `...  -.-- -.    ..     --`:
+.-....`...` .. `.         `.  --        `-  `.  :.
+./        `.`.-.                -         ---...: 
+ -          .:`                 .:```      :`   : 
+  :          -                   `:`        -  .- 
+  ..          -                    -        `.`-  
+   .-         -                    :-        -`:  
+   ./-`  `    -                     -        - /  
+   :.....:    -                     -       `..:` 
+     / .``-`. -                    -:     --/-..  
+      /----..-...-.   `            .. `..:./      
+      .. `.`.``    `.-.:-...`.......:``: :`:      
+       -.      ``````- :````.`-.....-..`  -`      
+        .-                              --        
+         `:                           .-`         
+          :                 ...       :           
+          :              `-`          :           
+          :               -           :           
+         -`               -           :           
+         :                -      `.`.`-.          
+         :         `..`...-   `..`    `.-`        
+         -`    `...`      `.-.`          .-       
+          /....`            ..            :       
+          :..               .:............`       
+             ...............                      
+
+	     )";
+const char * troll = R"(
+           -/++oooo+////::::://::-.`              
+         +mMMMNdddyhmmmmmmmmddhhhhhhyyyo+-        
+       .dMMNmmhdyydmmmmmdmMMMMMMmmmmmmdNMMNy:     
+      .NMMMNmhhdNMMNNMMMMhMMMMMMdNMMMMMmhMMMM-    
+     :NMMMMMMMmo:`   /+/odMMMMMMmNhyssshMMMMMh`   
+   -+yhhdmNhhN:////:://ss`sMMMho:  `-::/dmdmmmho` 
+ -smN//sso+/+shhy+:hMMMdsdMMMMMh dMMMMMMNyoosyyhs 
+`NhM.yMN/`odNmdddNMMMMNNdmMMMMMM:/hMMd/+-yhNdhhMy 
+ NhM`ds/`sds/:+ymMmmmmh`yshhmMMMMm``omMMMN`/MNhd: 
+ .hhyyMMy`/ymM.+o///oyh/hhhddMhyo:ymMNdms-` dmds  
+   :hNMMMd.-+: `odNMN+.++++/+oo//sso+:-+:+: sMm   
+    `yMMMMN+:m-/o:``:`/hmNMN mNm+.mNNo-m:.- sMd   
+      sMMMMMm/ hMMMm+.+:`                   yMd   
+       :dMMMMMNo:+dm.yMMMNh so++ .:- -: / :.NMd   
+         :hmdmmmmms/-+shmNd MMMm NM+:m-:o./mMMd   
+           `/shddydddmmhyso+ooo++oo+osyhmNNMMMm   
+               ./symmdyyhmmmmmddddmNNmmdddMNhMM   
+                   `:ohNNmmmmmmmmmmmmmmmmddmMMm   
+                        -/oyhmMMMMMMMMMMMMMMNs`   
+                               `-/+oosssso+-      
+			       )";
