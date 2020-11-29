3aef5011ced8fff0baaf8613dcdd9eea987cb405
zoomzer <66572381+zoomzer@users.noreply.github.com>
2020-11-29 02:41:13 +0100

    Add files via upload

---
+++ libs/words.h
@@ -0,0 +1,45 @@
+using namespace std;
+
+string words1[17] = {
+    "he ",
+    "them ",
+    "you ",
+    "me ",
+    "cum ",
+    "my dick ",
+    "the left ",
+    "jannies ",
+    "she ",
+    "redditors ",
+    "glowies ",
+    "british people ",
+    "black people ",
+    "white people ",
+    "anime girls ",
+    "jewish people ",
+    "bros ",
+};
+
+string words2[9] = {
+    "when ",
+    "existing ",
+    "in Syria, vibing with ",
+    "doing the needful ",
+    "riding with biden, ",
+    "on their way to ",
+    "engaging in cock and ball torture with ",
+    "taking away your penis by ",
+    "doing how to get your foreskin back strategy 1: ",
+};
+
+string words3[9] = {
+    "installing gentoo",
+    "cooming to anime girls",
+    "buttin' benis bin bagina :DDDDDD",
+    "shitposting on /g/",
+    "transitioning into an it",
+    "trying on programming socks",
+    "killing ",
+    "Segmentation fault (core dumped)",
+    "(do not research)",
+};
